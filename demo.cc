3b6a90897898c0ddc7c74628c289b6f7299adc4e
Gianni Masullo <gianni@Gianni-Masullos-MacBook.local>
2011-01-23 21:29:09 +0100

    indenting

--- demo.cc
+++ demo.cc
@@ -22,34 +22,34 @@ using namespace gui;
 #endif
 class MyEventReceiver : public IEventReceiver
 {
-public:
-	// This is the one method that we have to implement
-	virtual bool OnEvent(const SEvent& event)
-	{
-		// Remember whether each key is down or up
-		if (event.EventType == irr::EET_KEY_INPUT_EVENT) {
-      printf("key pressed '%d'\n",event.KeyInput.Key);
-			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+  public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event)
+    {
+      // Remember whether each key is down or up
+      if (event.EventType == irr::EET_KEY_INPUT_EVENT) {
+        printf("key pressed '%d'\n",event.KeyInput.Key);
+        KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+      }
+
+      return false;
     }
 
-		return false;
-	}
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+      return KeyIsDown[keyCode];
+    }
 
-	// This is used to check whether a key is being held down
-	virtual bool IsKeyDown(EKEY_CODE keyCode) const
-	{
-		return KeyIsDown[keyCode];
-	}
-	
-	MyEventReceiver()
-	{
-		for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
-			KeyIsDown[i] = false;
-	}
+    MyEventReceiver()
+    {
+      for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+        KeyIsDown[i] = false;
+    }
 
-private:
-	// We use this array to store the current state of each key
-	bool KeyIsDown[KEY_KEY_CODES_COUNT];
+  private:
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
 
 int main()
@@ -59,52 +59,52 @@ int main()
 
   GM_LOG("--------- starting gracing\n");
 
-	IrrlichtDevice *device =
-		createDevice( driverType, dimension2d<u32>(640, 480), 16,
-			false, false, false, &receiver);
+  IrrlichtDevice *device =
+    createDevice( driverType, dimension2d<u32>(640, 480), 16,
+        false, false, false, &receiver);
 
-	if (!device)
-		return 1;
+  if (!device)
+    return 1;
 
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+  device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
 
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
+  IVideoDriver* driver = device->getVideoDriver();
+  ISceneManager* smgr = device->getSceneManager();
+  IGUIEnvironment* guienv = device->getGUIEnvironment();
 
 
   CCrisMeshFileLoader * mloader=new CCrisMeshFileLoader(smgr,device->getFileSystem());
 
   smgr->addExternalMeshLoader(mloader);
 
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,260,22), true);
+  guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+      rect<s32>(10,10,260,22), true);
 
-	smgr->addCameraSceneNodeFPS();
+  smgr->addCameraSceneNodeFPS();
 
 
   Track * track = new Track(device,BASE_DIR "/track-1.zip");
 
 
   bool done=false;
-	while(device->run() && !done)
-	{
-		driver->beginScene(true, true, SColor(255,100,101,140));
+  while(device->run() && !done)
+  {
+    driver->beginScene(true, true, SColor(255,100,101,140));
 
-		smgr->drawAll();
-		guienv->drawAll();
+    smgr->drawAll();
+    guienv->drawAll();
 
-		driver->endScene();
-	  if(receiver.IsKeyDown(irr::KEY_ESCAPE)) {
+    driver->endScene();
+    if(receiver.IsKeyDown(irr::KEY_ESCAPE)) {
       done=true;
     }
-	}
+  }
 
-	device->drop();
+  device->drop();
 
-	return 0;
+  return 0;
 }
 
 /*
-That's it. Compile and run.
-**/
+   That's it. Compile and run.
+ **/
