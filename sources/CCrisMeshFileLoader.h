bcd0ca8d95c5a58c5e464aae4d1dd4ec7900f300
gianni <gianni.masullo@gmail.com>
2011-02-01 15:09:38 +0100

    added licence header to sources

--- sources/CCrisMeshFileLoader.h
+++ sources/CCrisMeshFileLoader.h
@@ -1,6 +1,19 @@
-// Copyright (C) 2002-2010 Nikolaus Gebhardt
-// This file is part of the "Irrlicht Engine".
-// For conditions of distribution and use, see copyright notice in irrlicht.h
+//  gracing - a idiot (but physically powered) racing game 
+//  Copyright (C) 2010 gianni masullo
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 3
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifndef CRIS_MESH_LOADER_H
 #define CRIS_MESH_LOADER_H
